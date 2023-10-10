4643ced985d103e07923d8c6a1e0720fb3eb0f1d
Quim Ortiz - Work <quimortiz21@gmail.com>
2023-10-10 09:54:58 +0200

    nicer example

--- test/python/rrt_free.py
+++ test/python/rrt_free.py
@@ -1,14 +1,10 @@
-
 import sys
 sys.path.append('.')
-
 
 import numpy as np
 from typing import Tuple, List
 import matplotlib.pyplot as plt
 import dynotree
-
-
 
 np.random.seed(0)
 
@@ -23,13 +19,10 @@ max_expansion = 1.
 def plot_robot(ax, x, color="black", alpha=1.0):
     ax.plot([x[0]], [x[1]], marker="o", color=color, alpha=alpha)
 
-
 def get_x_rand(): return np.random.rand(2) * (ub - lb) + lb
-
 
 ub = np.array([xlim[1], ylim[1]])
 lb = np.array([xlim[0], ylim[0]])
-
 
 tree = dynotree.TreeR2(-1)
 interpolate_fun = tree.interpolate
@@ -38,28 +31,21 @@ tree.addPoint(start, 0, True)
 valid_configs.append(start)
 parents = []
 parents.append(-1)
-
-
-
-__tmp = np.zeros(2)
 __xnew = np.zeros(2)
 for i in range(num_steps):
     xrand = get_x_rand()
     nn = tree.searchKnn(xrand, 1)[0]
     xnear = valid_configs[nn.payload]
     advance_rate = min(max_expansion / nn.distance, 1.)
-    print("advance_rate", advance_rate)
     interpolate_fun(xnear, xrand, advance_rate, __xnew)
     tree.addPoint(__xnew, len(valid_configs), True)
     valid_configs.append(np.copy(__xnew))
     parents.append(nn.payload)
 
-
 fig, ax = plt.subplots()
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
 ax.set_aspect('equal')
-
 
 for i, p in enumerate(parents):
     if i != -1:
